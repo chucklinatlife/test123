bf4ac02983fbf61168745785e9f8072a060f3146
Charles Lam <cklam19@gmail.com>
2014-07-18 00:10:43 +0000

    IT WORKS

--- flask_app.py
+++ flask_app.py
@@ -4,12 +4,22 @@ import random, os, os.path, json
 app = Flask(__name__)
 app.secret_key = 'This is really unique and secret'
 
-@app.route('/')
-def hello_person():
-    return """
-        <p>Who do you want me to say "Hi" to?</p>
-        <form method="POST" action="%s"><input name="person" /><input type="submit" value="Go!" /></form>
-        """ % (url_for('greet'),)
+@app.route('/invert')
+def invert():
+    key = request.args.get('key')
+    #print "key = %s, value = %s" % (key, value)
+    if os.path.isfile(key + '.txt'):
+        with open (key+'.txt', 'r') as file:
+            current_value = file.read()
+        if current_value == '1':
+            current_value = '0'
+        else:
+            current_value = '1'
+        with open (key+'.txt', 'w') as file:
+            file.write(current_value)
+        return current_value
+    else:
+        return "invalid"
 
 @app.route('/tem')
 def index():
