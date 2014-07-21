a8105c9b1527b342740eb4f7733bb8a94a870e45
Charles Lam <cklam19@gmail.com>
2014-07-21 17:24:32 +0000

    added delay

--- flask_app.py
+++ flask_app.py
@@ -39,6 +39,16 @@ def postdata():
         file.write(value+"\n")
     return ("written successfully with value %s" %(value))
 
+@app.route('/postdelaydata')
+def postDelayData():
+    key = request.args.get('key')
+    value=request.args.get('value')
+    print "key = %s, value = %s" % (key, value)
+    #writes value to a file
+    with open (key+'.txt', 'a') as file:
+        file.write(value+"\n")
+    return ("appended successfully with value %s" %(value))
+
 
 
 @app.route('/getdata')
@@ -58,4 +68,16 @@ def getdata():
     else:
         return 'invalid'
 
+@app.route('/getdelaydata')
+def getDelayData():
+    key = request.args.get('key')
+    #value = request.args.get('value')
+    #print "key = %s, value = %s" % (key, value)
+
+    #checks if the file exists, then reads existing file and returns the value
+    if os.path.isfile(key + '.txt'):
+        with open (key+'.txt', 'r') as file:
+            return file.read()
+    else:
+        return 'invalid'
 
